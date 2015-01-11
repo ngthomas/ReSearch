40140cee458162a8d8856eee577a50bfde0b4b3e
EC2 Default User <ec2-user@ip-10-169-79-22.ec2.internal>
2015-01-11 10:16:17 +0000

    allow unicode; google scholar is integrated with pubmed without hitch

--- search/search_core.py
+++ search/search_core.py
@@ -6,7 +6,9 @@ import scholar
 import argparse
 import json
 import search
-
+import urllib2
+import unicodedata
+from django.utils.encoding import smart_str, smart_unicode
 
 
 """
@@ -21,6 +23,7 @@ class GScholarItem:
             'Author':None,
             'Citations':0,
             'Excerpt':None,
+            'Journal':None,
         }
     def add_attrs(self, feature, value):
         self.attrs[feature] = value
@@ -44,6 +47,30 @@ def make_GScholarItems(query_search):
         GScholarItems.append(article)
     return GScholarItems
 
+def squash_unicode(s):
+    return unicodedata.normalize('NFKD', s).encode('ascii','ignore')
+
+def passTopubmed(GScholarItems):
+
+    for article in GScholarItems:
+        pmids = search.search_pubmed(article.attrs['Title'])
+        if len(pmids) > 0:
+            pmid = pmids[0]
+            pmr = search.get_pubmed(pmid)
+            
+            
+
+            article.attrs['Title'] = pmr.title
+            article.attrs['Journal']= pmr.journal
+
+            article.attrs['Author']=",".join(pmr.author_lst)
+
+            pmr.abstract = pmr.abstract.replace('"', "'")
+            article.attrs['Excerpt']=pmr.abstract
+
+
+
+
 
 '''
 This module prints out the list of google scholar literature in Json Format
@@ -55,7 +82,7 @@ def PrintInJson(scholar_obj):
    i=0;
    for article in scholar_obj:
         print "{",
-        print ",".join('"%s":"%s"' % (feature, article.attrs[feature]) for feature in article.attrs.keys()),
+        print ",".join('"%s":"%s"' % (feature, smart_str(article.attrs[feature])) for feature in article.attrs.keys()),
         print "}",
         i=i+1;
         if i<article_len:
@@ -66,9 +93,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-q','--query', default=None,type=str,help='query search', required=True)
     parser.add_argument('-r','--rating',default=1,type=str, help='Collect user"s rating', required=False)
+
+
     opts = parser.parse_args()
 
     GScholarItems = make_GScholarItems(opts.query)
+    passTopubmed(GScholarItems)
     PrintInJson(GScholarItems)
     #opts.query  ## need regular expression s/AND/+/g s/NOT/-/g 
 
